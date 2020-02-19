9611bbd36bee90086ec80f173a5be665fc91bfea
yoodb <afreon@163.com>
2020-02-19 12:29:09 +0800

    增加或修改demo01文件

--- springboot-study-demo01/src/main/java/com/yoodb/study/demo01/HelloWorldController.java
+++ springboot-study-demo01/src/main/java/com/yoodb/study/demo01/HelloWorldController.java
@@ -5,6 +5,10 @@ package com.yoodb.study.demo01;
 
 @RestController
 public class HelloWorldController {
+    /**
+     * 修改文件
+     * @return
+     */
     @RequestMapping("/hello")
     public String index() {
         return "关注微信公众号“Java精选”，Spring Boot系列文章持续更新中，带你从入门到精通，玩转Spring Boot框架。";
